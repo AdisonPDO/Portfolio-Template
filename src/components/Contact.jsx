cd7c4e13e425436ac4bebcc76af32cab5f89d267
AdisonP <52446207+AdisonPDO@users.noreply.github.com>
2024-02-29 08:32:38 +0100

    patch submit button size

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -123,8 +123,8 @@ const Contact = () => {
             className="live-demo flex justify-center sm:gap-4 
             gap-3 sm:text-[20px] text-[16px] text-timberWolf 
             font-bold font-beckman items-center py-5
-            whitespace-nowrap sm:w-[130px] sm:h-[50px] 
-            w-[100px] h-[45px] rounded-[10px] bg-night 
+            whitespace-nowrap sm:w-[150px] sm:h-[50px] 
+            w-[130px] h-[45px] rounded-[10px] bg-night 
             hover:bg-battleGray hover:text-eerieBlack 
             transition duration-[0.2s] ease-in-out"
             onMouseOver={() => {
